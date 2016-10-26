3ce967173d63b339ff8c6350d4c51f2c8a89c0fb
Nikhil Verma <anv.nikhil11@gmail.com>
2016-10-26 10:24:35 +0530

    first commit

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ module.exports = {
     'googleAuth' : {
         'clientID'      : '7864776390-hn4lbpsg2t346mmrv6i7e56h4uhuk9l1.apps.googleusercontent.com',
         'clientSecret'  : '4I_6ryKxUCooTYMppmn2nJny',
-        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+        'callbackURL'   : 'http://107.170.109.39:3000/auth/google/callback'
     }
 
 };
